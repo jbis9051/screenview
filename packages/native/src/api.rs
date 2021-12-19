2844ed4497d898bf61bf497b5be0236274baa940
jbis9051 <josh9051@gmail.com>
2021-12-18 23:19:42 -0500

    Adds some helpful cross-platform methods to the NativeAPI trait

--- packages/native/src/api.rs
+++ packages/native/src/api.rs
@@ -1,19 +1,75 @@
-use image::RgbImage;
+use image::{RgbImage};
 use std::error::Error;
 
-pub(crate) trait NativeCapture: Sized {
+struct Display {
+    id: u8,
+    name: str,
+    width: u32,
+    height: u32,
+}
+
+struct Window {
+    id: u8,
+    name: str,
+    width: u32,
+    height: u32,
+}
+
+struct MousePosition {
+    x: u32,
+    y: u32
+}
+
+type MouseButtonMask = u8;
+
+struct MouseScroll {
+    x: u32,
+    y: u32,
+}
+
+type Key = u32; // keysym
+
+type ClipboardType = str;
+
+type Frame = RgbImage;
+
+pub(crate) trait NativeAPI: Sized {
     type Error: Error;
 
     fn new() -> Result<Self, Self::Error>;
 
-    fn capture_screen(&self) -> Result<RgbImage, Self::Error>;
+    fn key_toggle(&self, key: &Key, down: &bool);
 
-    fn update_screen_capture(&self, cap: &mut RgbImage) -> Result<(), Self::Error> {
-        *cap = self.capture_screen()?;
+    fn pointer_position(&self) -> Result<MousePosition, Self::Error>;
+
+    fn set_pointer_position(&mut self) -> Result<(), Self::Error>;
+
+    fn click_mouse(&mut self, button_mask: &MouseButtonMask) -> Result<(), Self::Error>;
+
+    fn scroll_mouse(&mut self, scroll: &MouseScroll) -> Result<(), Self::Error>;
+
+    fn clipboard_types(&self) -> Result<(Vec<ClipboardType>), Self::Error>;
+
+    fn clipboard_content(&self, type_name: &ClipboardType) -> Result<([u8]), Self::Error>;
+
+    fn set_clipboard_content(&mut self, type_name: &ClipboardType, content: [u8]) -> Result<(), Self::Error>;
+
+    fn displays(&mut self) -> Result<(Vec<Display>), Self::Error>;
+
+    fn windows(&mut self) -> Result<(Vec<Window>), Self::Error>;
+
+    fn capture_display_frame(&self, display: &Display) -> Result<Frame, Self::Error>;
+
+    fn update_display_frame(&self, display: &Display, cap: &mut Frame) -> Result<(), Self::Error> {
+        *cap = self.capture_display_frame(display)?;
         Ok(())
     }
 
-    fn pointer_position(&self) -> Result<(u32, u32), Self::Error>;
+    fn capture_window_frame(&self, display: &Window) -> Result<Frame, Self::Error>;
 
-    fn key_toggle(&self, keysym: u32, down: bool);
+    fn update_window_frame(&self, window: &Window, cap: &mut Frame) -> Result<(), Self::Error> {
+        *cap = self.capture_window_frame(window)?;
+        Ok(())
+    }
+
 }
