15670636c53c8fb7759a6a9b136feee8ea529f63
jbis9051 <josh9051@gmail.com>
2022-04-12 15:00:58 -0400

    type stuff better

--- packages/rust/node-interop/index.node.d.ts
+++ packages/rust/node-interop/index.node.d.ts
@@ -1,8 +1,9 @@
 import type {InstanceType, ConnectionType} from "./index";
 
+
+export declare function new_instance<T extends InstanceType>(type: T): JSBox<Instance<T>>;
+export declare function connect<T extends InstanceType>(handle: JSBox<Instance<T>>, type: ConnectionType, addr: string): Promise<undefined>;
+
 // this is an opaque type, pointing to an object in rust memory
-export type JSBox = {};
-
-export declare function new_instance(type: InstanceType): JSBox;
-
-export declare function connect(handle: JSBox, type: ConnectionType, addr: string): JSBox;
+interface JSBox<T> { readonly __type: T; }
+interface Instance<T extends InstanceType> { readonly __type: unique symbol; } // Client or Host instance
