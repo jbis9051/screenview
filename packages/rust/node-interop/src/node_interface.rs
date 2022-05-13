bf89fcf1cdbb8bca720c556663768d45dbc6510c
jbis9051 <josh9051@gmail.com>
2022-05-13 11:28:23 -0400

    Macroafy node_interface.rs

--- packages/rust/node-interop/src/node_interface.rs
+++ packages/rust/node-interop/src/node_interface.rs
@@ -1,32 +1,58 @@
 use neon::{object::Object, prelude::*};
 use std::sync::Arc;
 
-struct VTable {
-    example_fn: Root<JsFunction>,
-    // TODO: add functions here
+macro_rules! vtable_arg_map {
+    (number) => {
+        i32
+    };
+    (string) => {
+        String
+    };
 }
 
-pub struct NodeInterface {
-    vtable: Arc<VTable>,
-}
+macro_rules! vtable_methods {
+    (
+        $(
+          $name: ident => (
+              $($arg: ident => $jstype: ident),*
+          )
+        ),*
+    ) => {
+        struct VTable {
+            $($name: Root<JsFunction>,)*
+        }
+        pub struct NodeInterface {
+            vtable: Arc<VTable>,
+        }
 
-impl NodeInterface {
-    pub fn from_obj<'a, C: Context<'a>>(cx: &mut C, obj: Handle<'_, JsObject>) -> NeonResult<Self> {
-        let example_fn = obj.get::<JsFunction, _, _>(cx, "example_fn")?.root(cx);
+        impl NodeInterface {
+            pub fn from_obj<'a, C: Context<'a>>(cx: &mut C, obj: Handle<'_, JsObject>) -> NeonResult<Self> {
+                $(let $name = obj.get::<JsFunction, _, _>(cx, stringify!($name))?.root(cx);)*
 
-        Ok(Self {
-            vtable: Arc::new(VTable { example_fn }),
-        })
+                Ok(Self {
+                    vtable: Arc::new(VTable {
+                        $($name,)*
+                    }),
+                })
+            }
+
+            $(
+                pub fn $name(&self, channel: &Channel $(,$arg: vtable_arg_map!($jstype))*){
+                    let vtable = Arc::clone(&self.vtable);
+
+                    channel.send(move |mut cx| {
+                        let func = vtable.$name.to_inner(&mut cx);
+                        let this = cx.null();
+                        let args = [$(cx.$jstype($arg).upcast(),)*];
+                        func.call(&mut cx, this, args).map(|_| ())
+                    });
+                }
+            )*
+        }
     }
-
-    pub fn example_fn(&self, channel: &Channel, arg1: String, arg2: i32) {
-        let vtable = Arc::clone(&self.vtable);
-
-        channel.send(move |mut cx| {
-            let func = vtable.example_fn.to_inner(&mut cx);
-            let this = cx.null();
-            let args = [cx.string(arg1).upcast(), cx.number(arg2).upcast()];
-            func.call(&mut cx, this, args).map(|_| ())
-        });
-    }
 }
+
+
+vtable_methods!(
+    example_fn => (arg1 => number, arg2 => string)
+);
